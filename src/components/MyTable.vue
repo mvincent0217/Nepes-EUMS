f20757a9c5c5fc6d26939172cc2b3b2c5c0eee8f
Christian Red Cerojano <CRSC@decatechnologies.com>
2023-03-08 14:55:39 +0800

    hide table

--- src/components/MyTable.vue
+++ src/components/MyTable.vue
@@ -28,28 +28,29 @@ export default {
 </script>
 
 <template>
-  <div> 
+  <div v-if="ButtonValue">
     <h3 class="TitleFont">{{ ButtonValue }}</h3>
-  <table>
-    <thead>
-      <tr class="ToprowFont">
-        <th>Equipment ID</th>
-        <th>MES State</th>
-        <th>EUMS State</th>
-        <th>Link</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-for="(data, index) in equipmentData" :key="index">
-        <td v-if="data.Productivity_State == ButtonNewVariable">{{ data.Equipment_ID }}</td>
-        <td v-if="data.Productivity_State == ButtonNewVariable">{{ data.MES_State }}</td>
-        <td v-if="data.Productivity_State == ButtonNewVariable">{{ data.EUMS_State }}</td>
- 
-        <td v-if="data.Productivity_State == ButtonNewVariable">
-          <button class="ViewEquipIDBtn" @click="emitEquipmentId(data.Equipment_ID)">View</button>
-        </td>
-      </tr>
-    </tbody>
-  </table>
-</div>
+    <table>
+      <thead>
+        <tr class="ToprowFont">
+          <th>Equipment ID</th>
+          <th>MES State</th>
+          <th>EUMS State</th>
+          <th>Link</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(data, index) in equipmentData" :key="index">
+          <td v-if="data.Productivity_State == ButtonNewVariable">{{ data.Equipment_ID }}</td>
+          <td v-if="data.Productivity_State == ButtonNewVariable">{{ data.MES_State }}</td>
+          <td v-if="data.Productivity_State == ButtonNewVariable">{{ data.EUMS_State }}</td>
+   
+          <td v-if="data.Productivity_State == ButtonNewVariable">
+            <button class="ViewEquipIDBtn" @click="emitEquipmentId(data.Equipment_ID)">View</button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
+
